00081408ba9c220d374fb8d145501690adc67084
Jérémy Lezmy <jeremy.lezmy-robert@hotmail.fr>
2022-06-03 17:48:28 +0200

    update

--- chapters/auto/07_scene.el
+++ chapters/auto/07_scene.el
@@ -4,22 +4,33 @@
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("subfiles" "../main/main.tex")))
    (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
    (TeX-run-style-hooks
     "latex2e"
     "subfiles"
     "subfiles10")
    (LaTeX-add-labels
-    "ch:res")
+    "ch:res"
+    "fig:scaleadapted"
+    "fig:projecthost"
+    "eq:backgroundcurved2"
+    "eq:psfmodelconstraint"
+    "fig:artefactssedmcube"
+    "tab:paramshypergal"
+    "eq:chi2hypergal"
+    "fig:metaslicescene"
+    "fig:corrmatrixmetaslice"
+    "eq:sigmahostchrom"
+    "fig:chromaticity_target")
    (LaTeX-add-bibliographies
     "99_references"))
  :latex)
