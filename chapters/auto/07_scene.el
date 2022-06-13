27277847c9d7308f0a78cbe659523d7c5f1ec830
Jérémy Lezmy <jeremy.lezmy-robert@hotmail.fr>
2022-06-13 17:04:49 +0200

    update

--- chapters/auto/07_scene.el
+++ chapters/auto/07_scene.el
@@ -4,16 +4,16 @@
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("subfiles" "../main/main.tex")))
    (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
     "subfiles"
@@ -36,13 +36,15 @@
     "fig:softl1"
     "eq:pullspectral"
     "fig:fullsceneZTF18accrorf"
+    "ssec:hostextract"
     "fig:hostspecZTF18accrorf"
+    "ssec:snextraction"
     "fig:targetisolatedZTF18accrorf"
     "fig:radialprofileZTF18accrorf"
     "fig:spectraZTF18accrorf"
     "fig:allsourcesZTF18accrorf"
-    "fig:fulldag")
-   (LaTeX-add-bibliographies
-    "99_references"))
+    "fig:fulldag"
+    "sec:snidclassification"
+    "fig:snidZTF18accrorf"))
  :latex)
 
